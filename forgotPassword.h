7377236cbc8e0597d287472094a9be31446e15ee
LezendarySandwich <mani.1@iitj.ac.in>
2019-11-24 03:21:46 +0530

    minor edits

--- forgotPassword.h
+++ forgotPassword.h
@@ -1,9 +1,6 @@
-#include<bits/stdc++.h>
 #include <Python.h>
+#include <bits/stdc++.h>
 using namespace std;
-// #include "header.h"
-// #include "sha256.h"
-// #include "User.h"
 
 void sendPasswordToEmail(string email, string new_password)
 {
@@ -12,11 +9,12 @@ void sendPasswordToEmail(string email, string new_password)
     FILE* pyfile;
     pyfile=fopen(filename,"r");
     string temp = "i=\""+email+"\"";
-    char temp1[temp.size()];
+    char temp1[temp.size()+1];
     for(int i=0;i<temp.size();++i)temp1[i]=temp[i];
+    temp1[temp.size()]='\0';
     temp = "j=\""+new_password+"\"";
     char temp2[temp.size()+1];
-    for(int i=0;i<temp.size();++i)temp2[i]=temp[i];
+    for(int i=0;i<temp.size();++i)temp2[i]=temp[i];cout<<temp<<' '<<temp2<<endl;
     temp2[temp.size()]='\0';
     PyRun_SimpleString(temp1);
     PyRun_SimpleString(temp2);
