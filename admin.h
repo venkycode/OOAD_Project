443d1304d0e4dbe2c82331a99dfad0ab472fb2fa
tanmay2625 <pandit.1@iitj.ac.in>
2019-11-24 21:46:52 +0530

    idk

--- admin.h
+++ admin.h
@@ -1,6 +1,4 @@
 #include <sqlite3.h>
-#include <chrono>
-#include <ctime>
 #include "header.h"
 #include "checks.h"
 #include "PasswordGenerator.h"
@@ -10,7 +8,6 @@ string temporaryID;       // also helps in adding transactions
 profile temporaryProfile; // helps in editing profile
 string temporaryPassword;
 string add;
-int temporaryOrderID; //used in assigning the order to the delivery person
 
 class admin
 {
@@ -379,12 +376,11 @@ public:
         return 0;
     }
 
-    void addTransaction(string id, bool isPaid, int moneyTransferred, int orderID, string payment_mode, string timeOfOrder)
+    void addTransaction(string id, bool isPaid, int moneyTransferred, int orderID)
     {
         string query = "SELECT TRANSACTIONS FROM USER_TRANSACTION WHERE ID = \'" + id + "\';";
-        add = to_string(orderID) + " | " + (isPaid ? "Paid" : "Refunded") + " | " + to_string(moneyTransferred) +\
-              " | " + payment_mode + " | " + timeOfOrder+ "\n";
-        temporaryID = id; 
+        add = to_string(orderID) + " | " + (isPaid ? "Paid" : "Refunded") + " | " + to_string(moneyTransferred) + "\n";
+        temporaryID = id;
         sqlite3_exec(DB, query.c_str(), update, NULL, NULL);
         exit = sqlite3_exec(DB, temporaryID.c_str(), callback, NULL, &messaggeError);
         if (exit != SQLITE_OK)
@@ -511,74 +507,15 @@ public:
  
     static int check_avail(void *data, int argc, char **argv, char **azColName)
     {
-        temporaryID = argv[1];
+        temporaryID = argv[0];
         return 0;
     }
  
-    int AssignedOrderId(string id){
-        temporaryOrderID = -1;
+    bool isAvailable(string id){
+        temporaryID = "#";
         string query = "SELECT * FROM ASSIGNED_ORDER WHERE ID = \'" + id + "\';";
         sqlite3_exec(DB, query.c_str(), check_avail, NULL, NULL);
-        return temporaryOrderID;
-    }
-
-    void payment(vector <order> Cart, enum mode payment_mode, string contact){
-        auto current_clock = chrono::system_clock::now();
-        time_t cur_time = std::chrono::system_clock::to_time_t(current_clock);
-        string currentTime = ctime(&cur_time);
-        string tempMode = "";
-        int totalCost = 0;
-        for(auto i: Cart){
-            totalCost += i.Product.price + i.Product.deliveryCharge;
-        }
-        cout<<"Total Cost =  "<<totalCost<<endl;
-        string cardNumber, expiry_Date, Cvv;
-        switch (payment_mode)
-        {
-        case cashOnDelivery:
-            tempMode = "CASH ON DELIVERY";
-            break;
-        case onlineBanking:
-            cout<<"Enter card number (****-****-****-****)"<<endl;
-            do{
-                cin>>cardNumber;
-            }while(!isCorrectCardNumber(cardNumber));
-            cout<<"Enter Expiry Date (mm/yy)"<<endl;
-            do{
-                cin>>expiry_Date;
-            }while(!isCorrectDate(expiry_Date));
-            cout<<"Enter Cvv (***)"<<endl;
-            do{
-                cin>>Cvv;
-            }while(isCorrectCvv(Cvv));
-            tempMode = "ONLINE BANKING";
-            break;
-        case Paytm:
-            cout<< "Do you wish to use your current number(Y/n): "<<contact<<endl;
-            char choice;
-            cin>>choice;
-            if(!(choice == 'Y' || choice == 'y')){
-                do{
-                    cin>>contact;
-                }while(isContactCorrect(contact));
-            }
-            tempMode = "PAYTM";
-            break;
-        case GooglePay:
-            cout<< "Do you wish to use your current number(Y/n): "<<contact<<endl;
-            char choice;
-            cin>>choice;
-            if(!(choice == 'Y' || choice == 'y')){
-                do{
-                    cin>>contact;
-                }while(isContactCorrect(contact));
-            }
-            tempMode = "GOOGLE PAY";
-            break;
-        default:
-            cerr<<"No such banking option"<<endl;
-            break;
-        }
+        return temporaryID == "#";
     }
 
 };
